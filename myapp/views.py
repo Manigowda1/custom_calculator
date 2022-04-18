e8069d2d270fabb6139193e4c7209454ae3da5c0
Manikanta Gowda B <78212802+Manigowda1@users.noreply.github.com>
2022-04-18 09:00:25 +0530

    Update views.py
    changes - int to float and rounded of numbers

--- myapp/views.py
+++ myapp/views.py
@@ -6,10 +6,10 @@ def home(request):
         bags = request.POST.get('bags')
         Ebag = request.POST.get('Ebag')
         # print(c_rate,bags)
-        cost_per_each_bag = int(c_rate)*68/100
-        total_cost = cost_per_each_bag*int(bags)
-        empty_bag_cost = (int(bags)*int(Ebag))
-        grand_total = total_cost - empty_bag_cost
+        cost_per_each_bag =round(float(c_rate)*68/100,2)
+        total_cost = round(cost_per_each_bag*float(bags),2)
+        empty_bag_cost = round((float(bags)*float(Ebag)),2)
+        grand_total = round((total_cost - empty_bag_cost),2)
         # print(f'Per bag ₹ :{cost_per_each_bag} and total cost :{total_cost} , after removal E bags {grand_total}')
         context = {'cost_per_bag': cost_per_each_bag,'total_cost':total_cost,'grand_total': grand_total,'bags':bags,'c_rate':c_rate,'Ebag': Ebag,'empty_bag_cost':empty_bag_cost}
         return render(request,'result.html',context)
